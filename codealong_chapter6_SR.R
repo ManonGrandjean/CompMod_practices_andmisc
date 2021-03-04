32203bc7b81a998fd0910e99687810a95df91bb1
ManonGrandjean <manon.eloise.albeck.grandjean@gmail.com>
2021-03-04 10:11:53 +0100

    code along SR 6

--- codealong_chapter6_SR.R
+++ codealong_chapter6_SR.R
@@ -1,1 +1,311 @@
 #code along SR 6
+
+# Overthinking: Simulated science distortion. 
+# Simulations like this one are easy to do in R, or in any other scripting language, once you have seen a few examples. 
+# In this simulation, we just draw some random Gaussian criteria for a sample of proposals and then select the top 10% combined scores.
+# R code 6.1
+set.seed(1914)
+N <- 200 # num grant proposals
+p <- 0.1 # proportion to select
+# uncorrelated newsworthiness and trustworthiness 
+nw <- rnorm(N)
+tw <- rnorm(N)
+# select top 10% of combined scores
+s<-nw+tw #totalscore
+q <- quantile( s , 1-p ) # top 10% threshold 
+selected <- ifelse( s >= q , TRUE , FALSE )
+cor( tw[selected] , nw[selected] )
+# I chose a specific seed so you can replicate the result in Figure 6.1, 
+# but if you rerun the simulation without the set.seed line, you’ll see there is nothing special about the seed I used.
+
+
+
+
+
+# 6.1.1. Multicollinear legs
+N <- 100                                        # number of individuals
+set.seed(909)
+height <- rnorm(N, 10, 2)                       # sim total height of each leg as proportion of height
+leg_prop <- runif(N, 0.4, 0.5)
+leg_left <- leg_prop*height +                   # sim left leg as proportion + error 
+  rnorm(N, 0, 0.02)
+leg_right <- leg_prop*height +                  # sim right leg as proportion + error 
+  rnorm(N, 0, 0.02)
+d <- data.frame(height, leg_left, leg_right)    # combine into dataframe
+
+library(rethinking)
+m6.1 <- quap(
+  alist(
+    height ~ dnorm( mu , sigma ) ,
+    mu <- a + bl*leg_left + br*leg_right ,
+    a ~ dnorm( 10 , 100 ) ,
+    bl ~ dnorm( 2 , 10 ) ,
+    br ~ dnorm( 2 , 10 ) ,
+    sigma ~ dexp( 1 )
+  ) , data=d )
+precis(m6.1)
+
+plot(precis(m6.1))
+
+post <- extract.samples(m6.1)
+plot( bl ~ br , post , col=col.alpha(rangi2,0.1) , pch=16 )
+
+sum_blbr <- post$bl + post$br
+dens( sum_blbr , col=rangi2 , lwd=2 , xlab="sum of bl and br" )
+
+
+
+
+
+# 6.1.2. Multicollinear milk.
+library(rethinking)
+data(milk)
+d <- milk
+d$K <- standardize( d$kcal.per.g )
+d$F <- standardize( d$perc.fat )
+d$L <- standardize( d$perc.lactose )
+
+# kcal.per.g regressed on perc.fat
+m6.3 <- quap(
+  alist(
+    K ~ dnorm( mu , sigma ) ,
+    mu <- a + bF*F ,
+    a ~ dnorm( 0 , 0.2 ) ,
+    bF ~ dnorm( 0 , 0.5 ) ,
+    sigma ~ dexp( 1 )
+  ) , data=d )
+# kcal.per.g regressed on perc.lactose
+m6.4 <- quap(
+  alist(
+    K ~ dnorm( mu , sigma ) ,
+    mu <- a + bL*L ,
+    a ~ dnorm( 0 , 0.2 ) ,
+    bL ~ dnorm( 0 , 0.5 ) ,
+    sigma ~ dexp( 1 )
+  ) , data=d )
+precis( m6.3 )
+precis( m6.4 )
+
+m6.5 <- quap(
+  alist(
+    K ~ dnorm( mu , sigma ) ,
+    mu <- a + bF*F + bL*L ,
+    a ~ dnorm( 0 , 0.2 ) ,
+    bF ~ dnorm( 0 , 0.5 ) ,
+    bL ~ dnorm( 0 , 0.5 ) ,
+    sigma ~ dexp( 1 )
+  ),
+  data=d )
+precis( m6.5 )
+
+# these two variables form essentially a single axis of variation. The easiest way to see this is to use a pairs plot:
+pairs( ~ kcal.per.g + perc.fat + perc.lactose , data=d , col=rangi2 )
+
+
+
+
+
+# Overthinking: Simulating collinearity.
+library(rethinking)
+data(milk)
+d <- milk
+sim.coll <- function( r=0.9 ) {
+  d$x <- rnorm( nrow(d) , mean=r*d$perc.fat ,
+                sd=sqrt( (1-r^2)*var(d$perc.fat) ) )
+  m <- lm( kcal.per.g ~ perc.fat + x , data=d )
+  sqrt( diag( vcov(m) ) )[2] # stddev of parameter
+}
+rep.sim.coll <- function( r=0.9 , n=100 ) {
+  stddev <- replicate( n , sim.coll(r) )
+  mean(stddev)
+}
+r.seq <- seq(from=0,to=0.99,by=0.01)
+stddev <- sapply( r.seq , function(z) rep.sim.coll(r=z,n=100) )
+plot( stddev ~ r.seq , type="l" , col=rangi2, lwd=2 , xlab="correlation" )
+
+
+
+
+
+# 6.2. Post-treatment bias
+set.seed(71)
+# number of plants
+N <- 100
+# simulate initial heights
+h0 <- rnorm(N,10,2)
+# assign treatments and simulate fungus and growth
+treatment <- rep( 0:1 , each=N/2 )
+fungus <- rbinom( N , size=1 , prob=0.5 - treatment*0.4 )
+h1 <- h0 + rnorm(N, 5 - 3*fungus)
+# compose a clean data frame
+d <- data.frame( h0=h0 , h1=h1 , treatment=treatment , fungus=fungus )
+precis(d)
+
+# 6.2.1. A prior is born.
+sim_p <- rlnorm( 1e4 , 0 , 0.25 )
+precis( data.frame(sim_p) )
+
+m6.6 <- quap(
+  alist(
+    h1 ~ dnorm( mu , sigma ),
+    mu <- h0*p,
+    p ~ dlnorm( 0 , 0.25 ),
+    sigma ~ dexp( 1 )
+  ), data=d )
+precis(m6.6)
+
+# Here’s the code to approximate the posterior:
+m6.7 <- quap(
+  alist(
+    h1 ~ dnorm( mu , sigma ),
+    mu <- h0 * p,
+    p <- a + bt*treatment + bf*fungus,
+    a ~ dlnorm( 0 , 0.2 ) ,
+    bt ~ dnorm( 0 , 0.5 ),
+    bf ~ dnorm( 0 , 0.5 ),
+    sigma ~ dexp( 1 )
+  ), data=d )
+precis(m6.7)
+
+# 6.2.2. Blocked by consequence.
+m6.8 <- quap(
+  alist(
+    h1 ~ dnorm( mu , sigma ),
+    mu <- h0 * p,
+    p <- a + bt*treatment,
+    a ~ dlnorm( 0 , 0.2 ),
+    bt ~ dnorm( 0 , 0.5 ),
+    sigma ~ dexp( 1 )
+  ), data=d )
+precis(m6.8)
+
+# 6.2.3. Fungus and d-separation
+library(dagitty)
+plant_dag <- dagitty( "dag {
+    H_0 -> H_1
+    F -> H_1
+    T -> F
+}")
+coordinates( plant_dag ) <- list( x=c(H_0=0,T=2,F=1.5,H_1=1) , y=c(H_0=0,T=0,F=0,H_1=0) )
+drawdag( plant_dag )
+
+impliedConditionalIndependencies(plant_dag)
+
+set.seed(71)
+N <- 1000
+h0 <- rnorm(N,10,2)
+treatment <- rep( 0:1 , each=N/2 )
+M <- rbern(N)
+fungus <- rbinom( N , size=1 , prob=0.5 - treatment*0.4 + 0.4*M )
+h1 <- h0 + rnorm( N , 5 + 3*M )
+d2 <- data.frame( h0=h0 , h1=h1 , treatment=treatment , fungus=fungus )
+
+
+
+
+
+# 6.3.1. Collider of false sorrow.
+# Here is the simulation design:
+# (1) Each year, 20 people are born with uniformly distributed happiness values.
+# (2) Each year, each person ages one year. Happiness does not change.
+# (3) At age 18, individuals can become married. The odds of marriage each year are proportional to an individual’s happiness.
+# (4) Once married, an individual remains married.
+# (5) After age 65, individuals leave the sample. (They move to Spain.)
+library(rethinking)
+d <- sim_happiness( seed=1977 , N_years=1000 )
+precis(d)
+
+d2 <- d[ d$age>17 , ] # only adults
+d2$A <- ( d2$age - 18 ) / ( 65 - 18 )
+
+d2$mid <- d2$married + 1
+m6.9 <- quap(
+  alist(
+    happiness ~ dnorm( mu , sigma ),
+    mu <- a[mid] + bA*A,
+    a[mid] ~ dnorm( 0 , 1 ),
+    bA ~ dnorm( 0 , 2 ),
+    sigma ~ dexp(1)
+  ) , data=d2 )
+precis(m6.9,depth=2)
+
+m6.10 <- quap(
+  alist(
+    happiness ~ dnorm( mu , sigma ),
+    mu <- a + bA*A,
+    a ~ dnorm( 0 , 1 ),
+    bA ~ dnorm( 0 , 2 ),
+    sigma ~ dexp(1)
+  ) , data=d2 )
+precis(m6.10)
+
+
+
+
+
+# 6.3.2. The haunted DAG
+N <- 200    # number of grandparent-parent-child triads
+b_GP <- 1   # direct effect of G on P
+b_GC <- 0   # direct effect of G on C
+b_PC <- 1   # direct effect of P on C
+b_U<-2      #direct effect of U on P and C
+
+set.seed(1)
+U <- 2*rbern( N , 0.5 ) - 1
+G <- rnorm( N )
+P <- rnorm( N , b_GP*G + b_U*U )
+C <- rnorm( N , b_PC*P + b_GC*G + b_U*U )
+d <- data.frame( C=C , P=P , G=G , U=U )
+
+m6.11 <- quap(
+  alist(
+    C ~ dnorm( mu , sigma ),
+    mu <- a + b_PC*P + b_GC*G,
+    a ~ dnorm( 0 , 1 ),
+    c(b_PC,b_GC) ~ dnorm( 0 , 1 ),
+    sigma ~ dexp( 1 )
+  ), data=d )
+precis(m6.11)
+
+m6.12 <- quap(
+  alist(
+    C ~ dnorm( mu , sigma ),
+    mu <- a + b_PC*P + b_GC*G + b_U*U,
+    a ~ dnorm( 0 , 1 ),
+    c(b_PC,b_GC,b_U) ~ dnorm( 0 , 1 ),
+    sigma ~ dexp( 1 )
+  ), data=d )
+precis(m6.12)
+
+
+
+
+
+# 6.4.1. Shutting the backdoor
+library(dagitty)
+dag_6.1 <- dagitty( "dag {
+    U [unobserved]
+    X -> Y
+    X <- U <- A -> C -> Y
+    U -> B <- C
+}")
+adjustmentSets( dag_6.1 , exposure="X" , outcome="Y" )
+
+
+
+
+
+# 6.4.3. Backdoor waffles
+library(dagitty)
+dag_6.2 <- dagitty( "dag {
+    A -> D
+    A -> M -> D
+    A <- S -> M
+    S -> W -> D
+}")
+adjustmentSets( dag_6.2 , exposure="W" , outcome="D" )
+
+impliedConditionalIndependencies( dag_6.2 )
+# This DAG is obviously not satisfactory—it assumes there are no unobserved confounds,
+# which is very unlikely for this sort of data
+
